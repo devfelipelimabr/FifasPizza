166207b04bd1a2fbc934335ef622e2b0eb08c948
devfelipelimabr <felipelima85@hotmail.com>
2023-11-15 11:33:46 -0300

    Created auth user middleware.

--- src/routes.ts
+++ src/routes.ts
@@ -2,11 +2,15 @@ import { Router } from "express";
 
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/AuthUserController";
+import { DetailUserController } from "./controllers/user/DetailUserController";
+
+import { isAuth } from "./middlewares/isAuth";
 
 const router = Router();
 
 // -- ROTAS USERS --
 router.post("/users", new CreateUserController().handle);
 router.post("/session", new AuthUserController().handle);
+router.get("/userinfo", isAuth, new DetailUserController().handle)
 
 export { router };
